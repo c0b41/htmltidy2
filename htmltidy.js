0b8538a347b12a2cc5f542ef81a2010e7b7aeda7
ayhankuru <ayhankuru@yandex.com.tr>
2017-03-30 21:01:12 +0300

    new binarys

--- htmltidy.js
+++ htmltidy.js
@@ -145,14 +145,10 @@ function chooseExec() {
       }
       break;
     case 'linux':
-      if(process.arch == 'x64'){
-        tidyExe = path.join('linux64/','tidy5');
-      } else {
-        tidyExe = path.join('linux32/','tidy5');
-      }
+        tidyExe = path.join('linux/','tidy');
       break;
     case 'darwin':
-      tidyExe = path.join('darwin', 'tidy5');
+      tidyExe = path.join('darwin', 'tidy');
       break;
     default:
       throw new Error('unsupported execution platform');
